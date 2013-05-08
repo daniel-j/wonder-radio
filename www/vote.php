7a971b0563220b7f7fddb1d3a79e0eac4c2aacd7
daniel-j <daniel.j.hede@gmail.com>
2013-05-08 14:01:35 +0200

    fixed vote

--- www/vote.php
+++ www/vote.php
@@ -2,14 +2,16 @@
 require_once "config.php";
 
 if (isset($_GET['vote']) && isset($_GET['trackId'])) {
-	$vote = intval($_GET['vote']) === 1? 1 : -1;
-	$trackId = intval($_GET['trackId']);
-	$sql = $db->prepare("
-		INSERT INTO
-			votes (trackId, ip, vote)
-		VALUES
-			(?,INET_ATON(?),?)
-		ON DUPLICATE KEY UPDATE
-			vote = ?");
-	$sql->execute(array($trackId, $_SERVER['REMOTE_ADDR'], $vote, $vote));
+	$vote = intval($_GET['vote']);
+	if ($vote >= -1 && $vote <= 1) {
+		$trackId = intval($_GET['trackId']);
+		$sql = $db->prepare("
+			INSERT INTO
+				votes (trackId, ip, vote)
+			VALUES
+				(?,INET_ATON(?),?)
+			ON DUPLICATE KEY UPDATE
+				vote = ?");
+		$sql->execute(array($trackId, $_SERVER['REMOTE_ADDR'], $vote, $vote));
+	}
 }
