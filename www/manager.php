164488f2d10e5cf56def4dcec038036fe5eb79ce
daniel-j <daniel.j.hede@gmail.com>
2013-05-10 01:31:38 +0200

    id3

--- www/manager.php
+++ www/manager.php
@@ -12,7 +12,7 @@ require_once('../getid3/getid3/getid3.php');
 // Initialize getID3 engine
 $getID3 = new getID3;
 
-$sql = $db->prepare("SELECT id, file, title, artist, album FROM tracks ORDER BY file LIMIT 1");
+$sql = $db->prepare("SELECT id, file, title, artist, album FROM tracks ORDER BY file LIMIT 10");
 $sql->execute();
 $tracks = $sql->fetchAll();
 
@@ -20,9 +20,9 @@ echo "<table border>";
 echo "<head><tr><th>Filename</th><th>Title</th><th>Artist</th><th>Album</th></tr></thead>";
 foreach($tracks as $track) {
 	$fileinfo = $getID3->analyze($musicPath.$track['file']);
-	print_r($fileinfo);
+	$id3 = $fileinfo['tags']['id3v2'];
 	echo "<tr><td rowspan=2>{$track['file']}</td><td>{$track['title']}</td><td>{$track['artist']}</td><td>{$track['album']}</td></tr>";
-	echo "<tr><td>{$track['title']}</td><td>{$track['artist']}</td><td>{$track['album']}</td></tr>";
+	echo "<tr><td>{$id3['title']}</td><td>{$id3['artist']}</td><td>{$id3['album']}</td></tr>";
 
 }
 echo "</table>";
