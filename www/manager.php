de4f5b6ed82d5bf9d7405ab2ac055e1dc7297554
daniel-j <daniel.j.hede@gmail.com>
2013-05-10 03:42:58 +0200

    id3

--- www/manager.php
+++ www/manager.php
@@ -7,23 +7,48 @@ if (!$state['admin']) {
 	exit;
 }
 
+// include getID3() library (can be in a different directory if full path is specified)
+require_once('../getid3/getid3/getid3.php');
+
+// Initialize getID3 engine
+$getID3 = new getID3;
 
 
 $songsPerPage = 10;
 
 if (isset($_POST['id']) && isset($_POST['file']) && isset($_POST['title']) && isset($_POST['artist']) && isset($_POST['album'])) {
 
-	$sql = $db->prepare("UPDATE tracks SET title = ?, artist = ?, album = ? WHERE id = ?");
-	$sql->execute($_POST['title'], $_POST['artist'], $_POST['album'], $_POST['id']);
+	//$sql = $db->prepare("UPDATE tracks SET title = ?, artist = ?, album = ? WHERE id = ?");
+	//$sql->execute($_POST['title'], $_POST['artist'], $_POST['album'], $_POST['id']);
+
+	/*require_once('../getid3/getid3/write.php');
+
+	$tagwriter = new getid3_writetags;
+	$tagwriter->filename = $musicPath.$_POST['file'];
+	$tagwriter->tagformats = array('id3v2.3');
+	$tagwriter->overwrite_tags = true;
+	$tagwriter->tag_encoding = "UTF-8";
+	$tagwriter->remove_other_tags = false;
+
+	$tagwriter->tag_data = array(
+		'title' => array($_POST['title']),
+		'artist' => array($_POST['artist']),
+		'album' => array($_POST['album'])
+	);
+
+	if ($tagwriter->WriteTags()) {
+		echo 'Successfully wrote tags<br>';
+		if (!empty($tagwriter->warnings)) {
+			echo 'There were some warnings:<br>'.implode('<br><br>', $tagwriter->warnings);
+		}
+	} else {
+		echo 'Failed to write tags!<br>'.implode('<br><br>', $tagwriter->errors);
+	}*/
+
+	echo "This feature has been disabled";
 
 	exit;
 }
-
-// include getID3() library (can be in a different directory if full path is specified)
-require_once('../getid3/getid3/getid3.php');
-
-// Initialize getID3 engine
-$getID3 = new getID3;
 
 $sql = $db->prepare("SELECT count(id) as totalTracks FROM tracks");
 $sql->execute();
@@ -84,8 +109,12 @@ echo "<thead><tr><th>Id</th><th>Filename</th><th width=1>Title</th><th width=1>A
 foreach($tracks as $track) {
 	$fileinfo = $getID3->analyze($musicPath.$track['file']);
 	$id3 = $fileinfo['tags']['id3v2'];
-	echo "<form method=\"post\"><input type=hidden name='id' value='{$track['id']}'><input type=hidden name='file' value='{$track['file']}'><tr><td rowspan=2>{$track['id']}</td><td rowspan=2>{$track['file']}</td><td>".$id3['title'][0]."</td><td>".$id3['artist'][0]."</td><td>".$id3['album'][0]."</td><td></td></tr>";
-	echo "<tr><td><input type=text name=title value=\"".htmlentities($track['title'])."\"></td><td><input type=text name=artist value=\"".htmlentities($track['artist'])."\"></td><td><input type=text name=album value=\"".htmlentities($track['album'])."\"></td><td><input type=\"submit\"</td></tr></form>";
+	$id3Title = !empty($id3['title']) ? $id3['title'][0] : "";
+	$id3Artist = !empty($id3['artist']) ? $id3['artist'][0] : "";
+	$id3Album = !empty($id3['album']) ? $id3['album'][0] : "";
+	
+	echo "<form method=\"post\"><input type=hidden name='id' value='{$track['id']}'><input type=hidden name='file' value='{$track['file']}'><tr><td rowspan=2>{$track['id']}</td><td rowspan=2>{$track['file']}</td><td>".$id3Title."</td><td>".$id3Artist."</td><td>".$id3Album."</td><td></td></tr>";
+	echo "<tr><td><input type=text name=title value=\"".htmlentities($id3Title)."\"></td><td><input type=text name=artist value=\"".htmlentities($id3Artist)."\"></td><td><input type=text name=album value=\"".htmlentities($id3Album)."\"></td><td><input type=\"submit\"</td></tr></form>";
 }
 echo "</table>";
 
